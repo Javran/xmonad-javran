8220d1501b7cb184a124e5c7b7ba46994bdc3c29
Javran Cheng <Javran.c@gmail.com>
2013-11-19 21:43:39 -0500

    add key mod + Fx to move window to a workspace

--- lib/JavranXMonad/Config.hs
+++ lib/JavranXMonad/Config.hs
@@ -7,6 +7,8 @@ import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.CustomKeys
 
+import qualified XMonad.StackSet as W
+
 initScript = "${HOME}/.xmonad/xmonad-init.sh"
 
 myManageHook = composeAll
@@ -21,7 +23,7 @@ myConfig = defaultConfig
     }
 
 insKeys :: XConfig l -> [((KeyMask, KeySym), X ())]
-insKeys conf@(XConfig {modMask = modm}) =
+insKeys conf@(XConfig {modMask = modm, workspaces = wkSpace}) =
     [ ((mod4Mask, xK_w          ) , spawn "firefox-bin") 
     , ((mod4Mask, xK_r          ) , spawn "xfce4-terminal")
     , ((mod4Mask, xK_e          ) , spawn "thunar")
@@ -33,3 +35,7 @@ insKeys conf@(XConfig {modMask = modm}) =
     , ((mod4Mask, xK_period     ) , spawn "mpc next")
     , ((mod4Mask, xK_slash      ) , spawn "mpc toggle")
     ]
+    ++ workspaceSwitchAltKeys modm wkSpace
+
+workspaceSwitchAltKeys modMask wkSpace =
+    [((modMask, k), windows $ W.shift i) | (i, k) <- zip wkSpace [xK_F1 .. xK_F12]]
