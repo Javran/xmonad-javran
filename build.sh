f770172b0d01904233ccef716ade379c2738082f
Javran Cheng <Javran.c@gmail.com>
2016-01-15 04:12:49 -0500

    use stack (ShakeBuild is broken)
    everything should work fine, except we still need some adjustment
    to ShakeBuild

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p _shake
-cabal exec -- ghc --make ShakeBuild.hs -rtsopts -with-rtsopts=-I0 \
+stack exec -- ghc --make ShakeBuild.hs -rtsopts -with-rtsopts=-I0 \
     -outputdir=_shake -o _shake/build \
     && _shake/build "$@"
