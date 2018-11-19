d9a965cde9514da5e8582daa6a4d7607dfdb4c5e
Javran Cheng <Javran.c@gmail.com>
2018-11-18 23:15:04 -0800

    need an overhaul of conky => SC => dzen

---
+++ src/XMonad/Javran/SystemInfoBar.hs
@@ -0,0 +1,13 @@
+module XMonad.Javran.SystemInfoBar where
+
+{-
+  WIP.
+
+  this module aims at eliminating the need for
+  current conky ==> StreamConverter ==> dzen pipeline,
+  simplifying it into a single SystemInfoBar,
+  in which it manages a separated instance of dzen.
+-}
+
+main :: IO ()
+main = pure ()
