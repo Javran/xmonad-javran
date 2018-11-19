d0fbc96c62bc6aea30fc37a2dd761337a9725f83
Javran Cheng <Javran.c@gmail.com>
2018-11-18 23:52:48 -0800

    TODOs

--- src/XMonad/Javran/SystemInfoBar.hs
+++ src/XMonad/Javran/SystemInfoBar.hs
@@ -9,5 +9,32 @@ module XMonad.Javran.SystemInfoBar where
   in which it manages a separated instance of dzen.
 -}
 
+{-
+  TODO
+
+  system info we are looking at:
+
+  - [ ] CPU usage for each individual ones
+
+    + http://e2e.ti.com/support/legacy_forums/embedded/linux/f/354/t/221192
+
+  - [ ] CPU freq
+  - [ ] memory usage
+  - [ ] network Rx & Tx
+  - [ ] mail checker
+  - [ ] mpd state
+  - [ ] whether battery is charging
+  - [ ] battery remaining
+  - [ ] date
+  - [ ] time
+
+  + stage 1 is to grab these info in a constant interval (say 1 sec)
+  + stage 2 is to have a process working on this, and impl another component
+    to render things in dzen-format
+
+
+
+ -}
+
 main :: IO ()
 main = pure ()
