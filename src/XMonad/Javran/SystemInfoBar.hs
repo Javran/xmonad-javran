e6fc2782ecbe882ac06d34f7fed2d8f4dbfcd7e8
Javran Cheng <Javran.c@gmail.com>
2018-11-19 23:32:41 -0800

    line parsing

--- src/XMonad/Javran/SystemInfoBar.hs
+++ src/XMonad/Javran/SystemInfoBar.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, TypeApplications, RecordWildCards #-}
 module XMonad.Javran.SystemInfoBar where
 
 import System.IO
 import Data.Function
+import Data.Char
+import Control.Monad
+import Text.ParserCombinators.ReadP
 
 {-
   WIP.
@@ -50,19 +53,31 @@ data CpuStatRow a = CpuStatRow
     -- there are actually 2 extra fields called "guest" and "guest_nice"
     -- in a recent version of kernel,
     -- but no word is given on how to deal with these two fields - guess we'll just ignore them.
-  } deriving (Functor)
+  } deriving (Functor, Show)
 
-parseRow :: String -> (CpuStatRow Int, String)
-parseRow = error "TODO"
+parseRow :: String -> ((String, CpuStatRow Int), String)
+parseRow raw = case readP_to_S parseLineP raw of
+    [(r, [])] -> r
+    _ -> error "parse error"
+  where
+    cpuDesc = munch1 (not . isSpace)
+    getInt = read @Int <$> munch1 isDigit
+    dataPart = do
+      [user, nice, system, idle, ioWait, irq, softIrq, steal] <-
+        replicateM 8 (skipSpaces >> getInt)
+      pure CpuStatRow {..}
+    parseLineP = (,) <$> ((,) <$> cpuDesc <*> dataPart) <*> munch (const True)
 
-getCpuStatRaw :: IO [CpuStatRow Int]
+getCpuStatRaw :: IO [(String, CpuStatRow Int)]
 getCpuStatRaw = do
   cpuRawLines <- withFile "/proc/stat" ReadMode $ \handle -> fix $ \kont -> do
     raw <- hGetLine handle
     if take 3 raw == "cpu"
       then (raw :) <$> kont
       else pure []
+  -- first "cpu ..." line is the total across all CPUs so we ignore it.
+  -- assume no parsing error, but we'll like to have leftover input just in case.
   pure (map (fst . parseRow) $ drop 1 cpuRawLines)
 
 main :: IO ()
-main = getCpuStatRaw >> pure ()
+main = getCpuStatRaw >>= print
