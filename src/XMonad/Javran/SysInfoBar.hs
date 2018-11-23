1a90c424f82f2546e7ba9a9da5977953c07c8c36
Javran Cheng <Javran.c@gmail.com>
2018-11-22 23:55:06 -0800

    fork on worker

--- src/XMonad/Javran/SysInfoBar.hs
+++ src/XMonad/Javran/SysInfoBar.hs
@@ -2,11 +2,23 @@ module XMonad.Javran.SysInfoBar where
 
 import XMonad.Javran.SysInfoBar.CpuUsage (CpuUsageWorker)
 import XMonad.Javran.SysInfoBar.Types
-import Data.Proxy
 import Data.Default
+import Data.Function
+import Data.Typeable
+import Control.Concurrent
 import Control.Concurrent.MVar
+import qualified Data.Map.Strict as M
 
 main :: IO ()
 main = do
-  mSt <- newMVar def
-  runWorker (Proxy :: Proxy CpuUsageWorker) mSt
+    mSt <- newMVar def
+    _ <- forkIO $ runWorker (Proxy :: Proxy CpuUsageWorker) mSt
+    fix $ \run -> do
+      threadDelay 500000
+      mv <- tryReadMVar mSt
+      case mv of
+        Just m
+          | k <- typeRep (Proxy :: Proxy CpuUsageWorker)
+          , (Just (SomeWorkerState s)) <- M.lookup k m -> pure ()
+        _ -> pure ()
+      run
