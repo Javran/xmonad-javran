e4dfb61aba2d822e5450bd4fc706cac8d419dfdd
Javran Cheng <Javran.c@gmail.com>
2018-11-23 23:29:27 -0800

    finish impl

--- src/XMonad/Javran/SysInfoBar/MemUsage.hs
+++ src/XMonad/Javran/SysInfoBar/MemUsage.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE TypeFamilies, RecordWildCards #-}
 module XMonad.Javran.SysInfoBar.MemUsage
-  (
+  ( MemUsageWorker
   ) where
 
 import System.IO
 import Control.Monad
 import Text.ParserCombinators.ReadP
+import XMonad.Javran.SysInfoBar.Types
 import Data.Char
+import Data.Typeable
+import Control.Concurrent
+import Data.Function
+import qualified Data.Map.Strict as M
 
 {-
   ref: <linux kernel source>/fs/meminfo.c
@@ -34,3 +40,22 @@ getMemInfoRaw = do
     parseRawField fieldName =
       string fieldName >> char ':' >> skipSpaces >>
       (read <$> munch1 isDigit) <* string "kB"
+
+runWorkerWith :: MVar BarState -> IO ()
+runWorkerWith mv = fix $ \run -> do
+  MemInfoRaw {..} <- getMemInfoRaw
+  let numer = mTotal - mAvailable
+      denom = mTotal
+      k = typeRep (Proxy :: Proxy MemUsageWorker)
+      res = SomeWorkerState (MemWState (numer, denom))
+  modifyMVar_ mv (pure . M.insert k res)  
+  threadDelay 1000000      
+  run
+
+data MemUsageWorker deriving Typeable
+
+instance Worker MemUsageWorker where
+  data WState MemUsageWorker = MemWState (Int, Int)
+  type WStateRep MemUsageWorker = (Int, Int)
+  runWorker _ = runWorkerWith
+  getStateRep (MemWState x) = x
