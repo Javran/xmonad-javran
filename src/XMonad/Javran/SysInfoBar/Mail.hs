d62f5e15cc18d24f4c8129664862ed33d9d9872e
Javran Cheng <Javran.c@gmail.com>
2018-11-30 23:52:57 -0800

    mail worker impl

--- src/XMonad/Javran/SysInfoBar/Mail.hs
+++ src/XMonad/Javran/SysInfoBar/Mail.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-module XMonad.Javran.SysInfoBar.Mail where
+{-# LANGUAGE TypeFamilies #-}
+module XMonad.Javran.SysInfoBar.Mail
+  ( MailWorker
+  ) where
 
 import System.IO
 import System.Environment
@@ -8,6 +10,11 @@ import Network.HaskellNet.IMAP
 import Network.HaskellNet.IMAP.Connection
 import Network.HaskellNet.IMAP.SSL
 import Control.Exception
+import XMonad.Javran.SysInfoBar.Types
+import Control.Concurrent
+import Data.Typeable
+import Data.Function
+import qualified Data.Map.Strict as M
 
 type AuthInfo = (String, String)
 
@@ -37,3 +44,37 @@ getUnreadMailCount :: IMAPConnection -> IO (Maybe Int)
 getUnreadMailCount c = catch getCount ioErrorHandler
   where
     getCount = Just . length <$> search c [NOTs (FLAG Seen)]
+
+
+runWorkerWith :: MVar BarState -> IO ()
+runWorkerWith mv = run Nothing
+  where
+    run :: Maybe IMAPConnection -> IO ()
+    run (Just conn) = fix $ \redo -> do
+      mResult <- getUnreadMailCount conn
+      case mResult of
+        Just _ -> do
+          let k = typeRep (Proxy :: Proxy MailWorker)  
+          modifyMVar_ mv (pure . M.insert k (SomeWorkerState (St mResult)))
+          sleep >> redo
+        Nothing ->
+          -- connection is down
+          sleep >> run Nothing
+    run Nothing = do
+      -- try to prepare a connection if possible
+      mc <- prepareConn
+      case mc of
+        Just _ -> run mc
+        Nothing -> sleep >> run Nothing
+
+    sleep :: IO ()
+    sleep = threadDelay (oneSec * 60 * 10)
+    oneSec =  1000000
+
+data MailWorker
+
+instance Worker MailWorker where
+  data WState MailWorker = St (Maybe Int)
+  type WStateRep MailWorker = Maybe Int
+  runWorker _ = runWorkerWith
+  getStateRep (St v) = v
