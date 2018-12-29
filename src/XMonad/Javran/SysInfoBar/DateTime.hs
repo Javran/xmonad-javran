f6dd938fa6703fb469c3fe130047ff2cb06087cb
Javran Cheng <Javran.c@gmail.com>
2018-12-28 23:44:23 -0800

    rendable date & time

--- src/XMonad/Javran/SysInfoBar/DateTime.hs
+++ src/XMonad/Javran/SysInfoBar/DateTime.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, OverloadedStrings #-}
 module XMonad.Javran.SysInfoBar.DateTime
   ( DateTime
   ) where
@@ -9,6 +9,9 @@ import Control.Concurrent
 import qualified Data.Map.Strict as M
 import Data.Time
 import Control.Monad
+import Data.Colour.SRGB
+import qualified System.Dzen as Dz
+import Data.String
 
 type DateTimeResult = (String, String)
 
@@ -29,3 +32,9 @@ instance Worker DateTime where
   type WStateRep DateTime = DateTimeResult
   runWorker _ = runWorkerWith
   getStateRep (St x) = x
+
+instance RenderableWorker DateTime where
+  wRender _ (dateStr, timeStr) = dStr <> " " <> tStr
+    where
+      dStr = Dz.fg (sRGB24read "#80FFFF") (fromString dateStr)
+      tStr = Dz.fg (sRGB24read "#FFFF80") (fromString timeStr)
