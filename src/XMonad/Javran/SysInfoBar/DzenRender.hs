382d1cc4f85d97626c579482197f63dd5ccd28c0
Javran Cheng <Javran.c@gmail.com>
2019-01-01 23:29:07 -0800

    experiment on type level

---
+++ src/XMonad/Javran/SysInfoBar/DzenRender.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE
+    ExplicitForAll
+  , LambdaCase
+  , ScopedTypeVariables
+  , OverloadedStrings
+  , TypeOperators
+  , TypeFamilies
+  #-}
+module XMonad.Javran.SysInfoBar.DzenRender where
+
+import System.Dzen
+import Data.Proxy
+import Data.String
+import Data.Typeable
+import Data.Monoid
+import Data.Colour.Names
+import Data.Maybe
+import Control.Monad
+
+import XMonad.Javran.SysInfoBar.Types
+import XMonad.Javran.SysInfoBar.CpuUsage (CpuUsage)
+import XMonad.Javran.SysInfoBar.CpuMaxFreq (CpuMaxFreq)
+import XMonad.Javran.SysInfoBar.MemUsage (MemUsage)
+import XMonad.Javran.SysInfoBar.TopProc (TopProc)
+import XMonad.Javran.SysInfoBar.NetStat (NetStat)
+import XMonad.Javran.SysInfoBar.Mail (Mail)
+import XMonad.Javran.SysInfoBar.Mpd (Mpd)
+import XMonad.Javran.SysInfoBar.Battery (Battery)
+import XMonad.Javran.SysInfoBar.DateTime (DateTime)
+
+renderCpuUsage :: [Int] -> DString
+renderCpuUsage xs = "[" <> foldMap rdr xs <> "]"
+  where
+    rdr :: Int -> DString
+    rdr n
+      | n >= 9 = fg red (if n > 9 then "X" else "9")
+      | n >= 5 = fg orange (fromString (show n))
+      | otherwise = fromString (show n)
+
+render :: forall w. Worker w => Proxy w -> WStateRep w -> DString
+render p st = fromMaybe fallback (getAlt result)
+  where
+    fallback :: DString
+    fallback = fromString (show (typeRep p))
+    result :: Alt Maybe DString
+    result = case eqT :: Maybe (w :~: CpuUsage)  of
+      Just Refl ->
+        -- observe that w ~ <CertainType> so that WStateRep w is a concrete type
+        Alt (Just (renderCpuUsage st))
+      Nothing -> mzero
