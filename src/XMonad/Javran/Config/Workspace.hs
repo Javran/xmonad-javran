96ebf28e0d8b1ad0a22c5535393f127b82ee0df1
Javran Cheng <Javran.c@gmail.com>
2019-01-06 22:54:20 -0800

    simplify workspace
    Map is really an overkill for this.

--- src/XMonad/Javran/Config/Workspace.hs
+++ src/XMonad/Javran/Config/Workspace.hs
@@ -4,20 +4,13 @@ module XMonad.Javran.Config.Workspace
   ) where
 
 import Data.Maybe (fromMaybe)
-import qualified Data.Map.Strict as M
 import XMonad.Core hiding (workspaces)
 
-workspaceNames :: [String]
-workspaceNames = words "any1 any2 msg1 ext1 ext2"
-
--- sadly WorkspaceId is just a type synonym to String,
--- so let's just use Int as String in hope of reducing computational work
--- when testing equalities
 workspaceIds :: [WorkspaceId]
-workspaceIds = zipWith (\x _ -> show x) [1 :: Int ..] workspaceNames
+workspaceIds = fst <$> workspaceItems
 
-workspaces :: M.Map WorkspaceId String
-workspaces = M.fromList (zip workspaceIds workspaceNames)
+workspaceItems :: [(WorkspaceId, String)]
+workspaceItems = zip (show <$> [1 :: Int ..]) $ words "any1 any2 msg1 ext1 ext2"
 
 workspaceName :: String -> String
-workspaceName n = fromMaybe "???" $ M.lookup n workspaces
+workspaceName n = fromMaybe "???" $ lookup n workspaceItems
